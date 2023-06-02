caf9c2ef80dbf6a8029468473066613b724346f5
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2023-06-02 06:52:40 -0600

    inicial

---
+++ build/controllers/CustomerVars.js
@@ -0,0 +1,16 @@
+let GlobalToken = 'MERCADOSEFECTIVOS';
+let GlobalCodSucursal = '';
+
+let GlobalEmpnit = '1034261-3';
+let GlobalEmpNombre = 'MERCADOS EFECTIVOS';
+
+
+let GlobalVentaMinima = 79.99;
+
+let dataEmpresas = [ 
+    {codsucursal:"ME-ZACAPA",nomsucursal:"SUCURSAL ZACAPA",color:"success"},
+    {codsucursal:"ME-IZABAL",nomsucursal:"SUCURSAL IZABAL",color:"primary"},
+    {codsucursal:"ME-JUTIAPA",nomsucursal:"SUCURSAL JUTIAPA",color:"info"},
+    {codsucursal:"ME-COBAN",nomsucursal:"SUCURSAL COBAN",color:"warning"},
+    {codsucursal:"ME-PETEN",nomsucursal:"SUCURSAL PETEN",color:"danger"},
+  ];
